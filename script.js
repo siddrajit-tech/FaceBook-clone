fd32fbb88ca3d092a45be46e095e06d9abdabab5
Siddharth Rajit <siddrajit@gmail.com>
2025-12-01 19:34:43 -0400

    Added dark mode toggle button and functionalilty

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const settingsMenu = document.querySelector(".settings-menu")
+const darkBtn = document.getElementById("dark-btn")
 
 
 function settingsMenuToggle() {
   settingsMenu.classList.toggle("settings-menu-height")
+}
+
+darkBtn.onclick = function() {
+  darkBtn.classList.toggle("dark-btn-on")
+  document.body.classList.toggle("dark-theme")
 }
